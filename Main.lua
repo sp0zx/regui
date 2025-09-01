af55480ece2aa1297c8d8b96af33d413480ce49f
Autumms <122081930+sp0zx@users.noreply.github.com>
2025-09-01 18:48:22 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 local Packages = table.freeze({
-	Blur = require("@self/Blur"),
-	ReGui = require("@self/ReGui"),
+	Blur = loadstring(game:HttpGet("https://raw.githubusercontent.com/sp0zx/regui/refs/heads/main/Packages/Blur.lua"))(),
+	ReGui = loadstring(game:HttpGet("https://raw.githubusercontent.com/sp0zx/regui/refs/heads/main/Packages/ReGui.lua"))(),
 })
 
 return Packages
